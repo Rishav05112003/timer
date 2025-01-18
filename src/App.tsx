89524f206d68644dcca4ae3d91904cfe3f7c7e86
Rishav05112003 <rishavdeep.skills@gmail.com>
2025-01-18 23:09:43 +0530

    change in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import moment from 'moment'
+import { useEffect, useState } from 'react';
+import './App.css';
+import moment from 'moment';
 
 function App() {
-  const [timer, setTime] = useState(moment.duration(10,'seconds'));
+  const [timer, setTimer] = useState(moment.duration(10, 'minutes'));
 
   useEffect(() => {
-    // repeatedly executing a function with fixed time delay
     const interval = setInterval(() => {
-      setTime(time => time.subtract(1,'seconds'))
-    },1000)
+      setTimer(prevTimer => {
+        // Subtract one second from the previous timer
+        const newTimer = moment.duration(prevTimer.asMilliseconds() - 1000);
+        return newTimer;
+      });
+    }, 1000);
 
-    return ()=> clearInterval(interval);
-  })
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    
     <>
-      <div className='countDown'>
-        
+      <div className="countDown">
+        <p>
+          Timer: {String(timer.minutes()).padStart(2, '0')} :{' '}
+          {String(timer.seconds()).padStart(2, '0')}
+        </p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
